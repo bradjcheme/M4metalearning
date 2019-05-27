7c5eeaca773f30a5eb867460f680a4df3052147d
Brad J. Johnson <bradj@andrew.cmu.edu>
2019-05-26 20:47:42 -0400

    Undo opt MAE option for ETS

--- R/forec_methods_list.R
+++ R/forec_methods_list.R
@@ -45,7 +45,7 @@ auto_arima_forec <- function(x, h) {
 #' @describeIn forec_methods forecast::ets
 #' @export
 ets_forec <- function(x, h) {
-  model <- forecast::ets(x, opt.crit="mae")
+  model <- forecast::ets(x) # undo opt.crit="mae" change for obc implementation
   forecast::forecast(model, h=h)$mean
 }
 
